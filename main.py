bf8320b80f7df822822d663b41843437b71736c7
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-08-24 11:04:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import logging
 import os
 
 import hydra
+import torch
 from hydra.utils import instantiate
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
@@ -12,7 +14,28 @@ def main(config: DictConfig):
     logger.debug("Working directory : {}".format(os.getcwd()))
 
     dataset = instantiate(config.data)
-    print(dataset.load("train"))
+    train = dataset.load("train")
+
+    train_simulator = instantiate(config.train_simulator)
+    train_clicks = train_simulator(train)
+    val_clicks = train_simulator(train)  # Fixme: Relative generation
+
+    test_simulator = instantiate(config.test_simulator)
+    test_clicks = test_simulator(train)
+
+    train_loader = instantiate(config.train_loader, dataset=train_clicks)
+    val_loader = instantiate(config.test_loader, dataset=val_clicks)
+    test_loader = instantiate(config.test_loader, dataset=test_clicks)
+
+    trainer = instantiate(config.trainer)
+    model = instantiate(config.model)
+
+    trainer.fit(model, train_loader, val_loader)
+    trainer.test(model, test_loader)
+
+    logging.debug(
+        f"Inferred examination probability: {model.examination(torch.arange(10))}"
+    )
 
 
 if __name__ == "__main__":
