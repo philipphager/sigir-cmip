e3b856b404f3b68d86646e85aa46d22b5f74f5f7
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-10-12 16:57:21 +0200

    Update powerlaw.py

--- src/simulation/query_dist/powerlaw.py
+++ src/simulation/query_dist/powerlaw.py
@@ -7,12 +7,10 @@ from src.simulation.query_dist.base import QueryDist
 class PowerLawQueryDist(QueryDist):
     def __init__(self, alpha: float):
         self.alpha = alpha
-        self.gen = (
-            torch.Generator()
-        )  # Important in order to get the same distribution at training and test time
+        self.generator = torch.Generator()  # Fix distribution at training and test time
 
     def __call__(self, n_queries: int, n_sessions: int) -> torch.LongTensor:
-        shuffle_q = torch.randperm(n_queries, generator=self.gen)
+        shuffle_q = torch.randperm(n_queries, generator=self.generator)
         probs = torch.arange(1, n_queries + 1).pow(-self.alpha)
         probs = probs[shuffle_q]
         return Categorical(probs=probs).sample(sample_shape=(n_sessions,))
