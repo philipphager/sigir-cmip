1d58e5e15f80b8da1ed34ba65cf0cd9d6789bded
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-10-16 20:31:53 +0200

    Update drCTR

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,6 +2,7 @@ from .base import ClickModel
 from .cacm_minus import CACM_minus
 from .dbn import DBN
 from .dctr import DCTR
+from .dctr import RankedDCTR
 from .ncm import NCM
 from .pbm import PBM
 from .ubm import UBM
--- src/model/dctr.py
+++ src/model/dctr.py
@@ -8,23 +8,40 @@ from ..evaluation.metrics import get_click_metrics
 
 
 class DCTR(ClickModel):
+    """
+    dCTR model as in [Deffayet 2022] with Beta(alpha=1, beta=1) prior
+    """
+
     def __init__(
         self,
         loss: nn.Module,
         optimizer: str,
         learning_rate: float,
         n_documents: int,
+        prior_clicks: int,
+        prior_impressions: int,
     ):
         super().__init__(loss, optimizer, learning_rate)
+        # Turn off optimization for count-based click model
         self.automatic_optimization = False
-        self.clicks = nn.Parameter(torch.zeros(n_documents), requires_grad=False)
-        self.impressions = nn.Parameter(torch.zeros(n_documents), requires_grad=False)
+
+        self.clicks = nn.Parameter(
+            torch.full((n_documents,), prior_clicks, dtype=torch.float),
+            requires_grad=False,
+        )
+        self.impressions = nn.Parameter(
+            torch.full((n_documents,), prior_impressions, dtype=torch.float),
+            requires_grad=False,
+        )
 
     def training_step(self, batch, idx):
         q, x, y, y_click, n = batch
 
-        self.clicks[x] += y_click
-        self.impressions[x] += 1
+        # Ignore padding
+        impressions = (x > 0).float()
+        # CTR per document
+        self.clicks.index_add_(0, x.ravel(), y_click.ravel())
+        self.impressions.index_add_(0, x.ravel(), impressions.ravel())
 
         y_predict_click, y_predict = self.forward(x, true_clicks=y_click)
         loss = self.loss(y_predict_click, y_click, n)
@@ -41,9 +58,87 @@ class DCTR(ClickModel):
         click_pred: bool = True,
         true_clicks: torch.LongTensor = None,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        y_predict = self.clicks[x] / self.impressions[x].clip(min=1)
+        y_predict = self.clicks[x] / self.impressions[x]
 
         if click_pred:
             return y_predict, y_predict
         else:
             return y_predict
+
+
+class RankedDCTR(ClickModel):
+    """
+    drCTR model as in [Deffayet 2022] with Beta(alpha=1, beta=1) prior
+    """
+
+    def __init__(
+        self,
+        loss: nn.Module,
+        optimizer: str,
+        learning_rate: float,
+        n_documents: int,
+        n_results: int,
+        prior_clicks: int,
+        prior_impressions: int,
+    ):
+        super().__init__(loss, optimizer, learning_rate)
+        # Turn off optimization for count-based click model
+        self.automatic_optimization = False
+        self.document_clicks = nn.Parameter(
+            torch.full((n_documents * n_results,), prior_clicks, dtype=torch.float),
+            requires_grad=False,
+        )
+        self.document_impressions = nn.Parameter(
+            torch.full(
+                (n_documents * n_results,), prior_impressions, dtype=torch.float
+            ),
+            requires_grad=False,
+        )
+        self.rank_clicks = nn.Parameter(torch.zeros(n_results), requires_grad=False)
+        self.rank_impressions = nn.Parameter(
+            torch.zeros(n_results), requires_grad=False
+        )
+
+    def training_step(self, batch, idx):
+        q, x, y, y_click, n = batch
+        n_batch, n_items = x.shape
+
+        # Ignore padding
+        impressions = (x > 0).float()
+        # CTR per rank
+        self.rank_clicks += y_click.sum(dim=0)
+        self.rank_impressions += impressions.sum(dim=0)
+        # CTR per document and rank
+        ranks = torch.arange(n_items, device=self.device).repeat(n_batch, 1)
+        idx = (x * n_items + ranks).ravel()
+        self.document_clicks.index_add_(0, idx, y_click.ravel())
+        self.document_impressions.index_add_(0, idx, impressions.ravel())
+
+        y_predict_click, _ = self.forward(x, true_clicks=y_click)
+        loss = self.loss(y_predict_click, y_click, n)
+
+        metrics = get_click_metrics(y_predict_click, y_click, n, "train_")
+        metrics["train_loss"] = loss
+        self.log_dict(metrics)
+
+        return loss
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        click_pred: bool = True,
+        true_clicks: torch.LongTensor = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        n_batch, n_items = x.shape
+
+        ranks = torch.arange(n_items, device=self.device).repeat(n_batch, 1)
+        idx = x * n_items + ranks
+
+        rank_ctr = self.rank_clicks / self.rank_impressions
+        y_predict = self.document_clicks[idx] / self.document_impressions[idx]
+        relevance = 1 / rank_ctr * y_predict
+
+        if click_pred:
+            return y_predict, relevance
+        else:
+            return relevance
