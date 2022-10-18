14fb50f61a53d79a7ee49d6c2d2828b00494b21a
Romain <romain.deffayet@naverlabs.com>
2022-10-18 08:49:47 +0200

    Fixed device issue in perplexity computation.

--- src/data/base.py
+++ src/data/base.py
@@ -52,7 +52,8 @@ class DatasetLoader(ABC):
         """
         Download directory for raw dataset .zip files
         """
-        path = self.base_dir / "download"
+        path = Path("/local/download")
+        # path = self.base_dir / "download"
         path.mkdir(parents=True, exist_ok=True)
         return path
 
