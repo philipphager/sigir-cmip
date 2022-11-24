Agreement ratio works (old formula) + minor fixes and decreased learning rate to allow for multiple epoch training.

src/evaluation/metrics.py
@@ -106,7 +106,12 @@ def agreement_ratio(
     lp_wrong = torch.logical_xor(true_pref, lp_pref)
     cm_wrong = torch.logical_xor(true_pref, cm_pref)
 
-    return torch.logical_and(lp_wrong, cm_wrong).sum() / lp_wrong.sum().clip(min=1)
+    agreement_ratio = (
+        torch.logical_and(lp_wrong, cm_wrong).sum()
+        / torch.logical_or(lp_wrong, cm_wrong).sum()
+    )
+
+    return agreement_ratio, torch.cat(pairs, dim=1).shape[1]
 
 
 def _get_disjoint_pairs(n: torch.LongTensor) -> List[torch.Tensor]:
@@ -179,9 +184,12 @@ def get_relevance_metrics(
         f"{prefix}ndcg{k}": batch_ndcg[i] for i, k in enumerate(["@1", "@5", "@10", ""])
     }
     if y_lp is not None:
-        batch_agreement_ratio = agreement_ratio(
+        batch_agreement_ratio, batch_n_pairs = agreement_ratio(
             y_predict, y_lp, y_true, n, disjoint_pairs=False
         )
-        relevance_metrics |= {f"{prefix}agreement_ratio": batch_agreement_ratio}
+        relevance_metrics |= {
+            f"{prefix}agreement_ratio": batch_agreement_ratio,
+            "n_pairs": batch_n_pairs,
+        }
 
     return relevance_metrics

src/model/base.py
@@ -76,16 +76,37 @@ class ClickModel(pl.LightningModule):
 
             metrics = get_click_metrics(y_predict_click, y_click, n, "test_clicks_")
             metrics["test_loss"] = loss
+            self.log_dict(metrics)
         else:
             # Rating dataset
             query_ids, x, y, n = batch
-            print(query_ids)
             y_predict = self.forward(x, click_pred=False)
-            y_lp = self.lp_scores[query_ids].to(self.device)
+            y_lp = self.lp_scores[idx * len(query_ids) : (idx + 1) * len(query_ids)].to(
+                self.device
+            )
             metrics = get_relevance_metrics(y_predict, y, y_lp, n, "test_rels_")
+            self.log_dict(
+                {
+                    key: val
+                    for key, val in metrics.items()
+                    if key not in ["test_rels_agreement_ratio", "n_pairs"]
+                }
+            )
 
-        # self.log_dict(metrics)
         return metrics
 
     def test_epoch_end(self, outputs):
-        print(outputs)
+        # Weighted sum to account for different number of pairs in different batches
+        agreement_ratios = torch.stack(
+            [metrics["test_rels_agreement_ratio"] for metrics in outputs[1]]
+        )
+        n_pairs = torch.stack(
+            [
+                torch.tensor(metrics["n_pairs"], device=self.device)
+                for metrics in outputs[1]
+            ]
+        )
+        test_agreement_ratio = torch.sum(agreement_ratios * n_pairs) / torch.sum(
+            n_pairs
+        )
+        self.log("test_rels_agreement_ratio", test_agreement_ratio)