008d0c6d6b796b72734f40ecd6c901c812a5fc20
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-12-08 12:26:31 +0100

    Update dctr-based click models

--- src/model/dctr.py
+++ src/model/dctr.py
@@ -6,18 +6,18 @@ from scipy.stats import beta
 from torch import nn
 
 from ..data.dataset import ClickDatasetStats
-from .base import ClickModel
+from .base import StatsClickModel
 
 logger = logging.getLogger(__name__)
 
 
 def fit_beta(y: torch.Tensor, eps: float = 1e-4):
-    ctr = y.clip(min=eps, max=1 - eps).cpu().numpy()
+    ctr = y.clip(min=eps, max=1 - eps).detach().cpu().numpy()
     a, b, _, _ = beta.fit(ctr, method="MLE", floc=0, fscale=1)
     return a, b
 
 
-class DCTR(ClickModel):
+class DCTR(StatsClickModel):
     """
     dCTR model as in [Deffayet 2022], computing relevance as CTR per document.
     We smooth CTRs using empirical bayes, estimating priors for a beta distribution
@@ -27,42 +27,19 @@ class DCTR(ClickModel):
     def __init__(
         self,
         loss: nn.Module,
-        optimizer: str,
-        learning_rate: float,
-        n_documents: int,
-        train_stats: ClickDatasetStats = None,
+        train_stats: ClickDatasetStats,
         lp_scores: torch.FloatTensor = None,
+        **kwargs,
     ):
-        super().__init__(loss, optimizer, learning_rate, lp_scores)
-        # Turn off optimization for count-based click model
-        self.automatic_optimization = False
-        self.n_documents = n_documents
-        self.train_stats = train_stats
-        # Init priors to 1.0, avoid division by zero in validation before training
-        self.prior_clicks = nn.Parameter(
-            torch.zeros(1, dtype=torch.float),
-            requires_grad=False,
-        )
-        self.prior_impressions = nn.Parameter(
-            torch.zeros(1, dtype=torch.float),
-            requires_grad=False,
-        )
-        self.clicks = nn.Parameter(
-            torch.zeros(n_documents, dtype=torch.float),
-            requires_grad=False,
-        )
-        self.impressions = nn.Parameter(
-            torch.zeros(n_documents, dtype=torch.float),
-            requires_grad=False,
-        )
+        super().__init__(loss, train_stats, lp_scores)
 
-    def on_train_start(self):
+    def setup_parameters(self, train_stats: ClickDatasetStats):
         # Sum clicks and impressions per document over all ranks
-        clicks = self.train_stats.document_rank_clicks
-        self.clicks += clicks.sum(dim=1).to(self.device)
+        clicks = train_stats.document_rank_clicks.to(self.device)
+        self.clicks = nn.Parameter(clicks.sum(dim=1))
 
-        impressions = self.train_stats.document_rank_impressions
-        self.impressions += impressions.sum(dim=1).to(self.device)
+        impressions = train_stats.document_rank_impressions.to(self.device)
+        self.impressions = nn.Parameter(impressions.sum(dim=1))
 
         # Compute CTRs for documents that got at least one impression
         ctr = self.clicks / self.impressions.clip(min=1)
@@ -70,14 +47,9 @@ class DCTR(ClickModel):
 
         # Fit beta prior on CTRs, A being clicks and B non-clicks
         a, b = fit_beta(ctr)
-        self.prior_clicks += a
-        self.prior_impressions += a + b
+        self.prior_clicks = a
+        self.prior_impressions = a + b
         logger.info(f"dCTR with Beta({a}, {b}), prior CTR per document: {a / (a + b)}")
-
-    def training_step(self, batch, idx):
-        # Update global_step counter for checkpointing
-        self.optimizers().step()
-        return super().training_step(batch, idx)
 
     def forward(
         self,
@@ -95,7 +67,7 @@ class DCTR(ClickModel):
             return y_predict
 
 
-class RankedDCTR(ClickModel):
+class RankedDCTR(StatsClickModel):
     """
     drCTR model as in [Deffayet 2022], computing relevance as CTR per document and rank.
     We smooth CTRs using empirical bayes, estimating priors for a beta distribution
@@ -106,61 +78,27 @@ class RankedDCTR(ClickModel):
     def __init__(
         self,
         loss: nn.Module,
-        optimizer: str,
-        learning_rate: float,
-        n_documents: int,
-        n_results: int,
         train_stats: ClickDatasetStats,
         lp_scores: torch.FloatTensor = None,
+        **kwargs,
     ):
-        super().__init__(loss, optimizer, learning_rate, lp_scores)
-        # Turn off optimization for count-based click model
-        self.automatic_optimization = False
-        self.n_documents = n_documents
-        self.n_result = n_results
-        self.train_stats = train_stats
-        self.prior_clicks = nn.Parameter(
-            torch.zeros(n_results),
-            requires_grad=False,
-        )
-        self.prior_impressions = nn.Parameter(
-            torch.zeros(n_results),
-            requires_grad=False,
-        )
-        self.clicks = nn.Parameter(
-            torch.zeros((n_documents, n_results), dtype=torch.float),
-            requires_grad=False,
-        )
-        self.impressions = nn.Parameter(
-            torch.zeros((n_documents, n_results), dtype=torch.float),
-            requires_grad=False,
-        )
-        self.rank_clicks = nn.Parameter(
-            torch.zeros(n_results),
-            requires_grad=False,
-        )
-        self.rank_impressions = nn.Parameter(
-            torch.zeros(n_results),
-            requires_grad=False,
-        )
+        super().__init__(loss, train_stats, lp_scores)
 
-    def on_train_start(self):
-        clicks = self.train_stats.document_rank_clicks
-        clicks = clicks.to(self.device)
+    def setup_parameters(self, train_stats: ClickDatasetStats):
+        clicks = train_stats.document_rank_clicks.to(self.device)
+        impressions = self.train_stats.document_rank_impressions.to(self.device)
 
-        impressions = self.train_stats.document_rank_impressions
-        impressions = impressions.to(self.device)
+        self.clicks = nn.Parameter(clicks)
+        self.impressions = nn.Parameter(impressions)
+        self.rank_clicks = nn.Parameter(clicks.sum(dim=0))
+        self.rank_impressions = nn.Parameter(impressions.sum(dim=0))
 
-        self.clicks += clicks
-        self.impressions += impressions
-        self.rank_clicks += clicks.sum(dim=0)
-        self.rank_impressions += impressions.sum(dim=0)
-
-        prior_clicks = torch.zeros(self.n_result)
-        prior_impressions = torch.zeros(self.n_result)
+        n_results = len(self.rank_clicks)
+        prior_clicks = torch.zeros(n_results).to(self.device)
+        prior_impressions = torch.zeros(n_results).to(self.device)
 
         # Fit beta prior on document CTRs per rank i
-        for i in range(self.n_result):
+        for i in range(n_results):
             ctr = self.clicks[:, i] / self.impressions[:, i].clip(min=1)
             ctr = ctr[self.impressions[:, i] > 0]
 
@@ -171,13 +109,8 @@ class RankedDCTR(ClickModel):
                 f"ranked-dCTR with Beta({a}, {b}), prior CTR at rank {i}: {a / (a + b)}"
             )
 
-        self.prior_clicks += prior_clicks.to(self.device)
-        self.prior_impressions += prior_impressions.to(self.device)
-
-    def training_step(self, batch, idx):
-        # Update global_step counter for checkpointing
-        self.optimizers().step()
-        return super().training_step(batch, idx)
+        self.prior_clicks = nn.Parameter(prior_clicks)
+        self.prior_impressions = nn.Parameter(prior_impressions)
 
     def forward(
         self,
