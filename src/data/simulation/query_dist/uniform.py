a012650ddada0177e9fc83a34e2c0017a6504acb
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-01-02 18:00:10 +0100

    Fix seed in uniform query distribution

--- src/data/simulation/query_dist/uniform.py
+++ src/data/simulation/query_dist/uniform.py
@@ -1,14 +1,13 @@
 import torch
-from torch.distributions.categorical import Categorical
-
-from src.data.simulation.query_dist.base import QueryDist
 
 
-class UniformQueryDist(QueryDist):
-    def __init__(self):
-        return
+class UniformQueryDist:
+    def __init__(self, random_state: int):
+        self.generator = torch.Generator().manual_seed(random_state)
 
     def __call__(self, n_queries: int, n_sessions: int) -> torch.LongTensor:
-        return Categorical(probs=torch.ones(n_queries)).sample(
-            sample_shape=(n_sessions,)
-        )
+        return torch.randint(
+            n_queries,
+            (n_sessions,),
+            generator=self.generator,
+        ).long()
