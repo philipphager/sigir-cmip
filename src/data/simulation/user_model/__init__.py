34ac06ed796d997bf498e400cf187d8a5dbd709a
Romain <romain.deffayet@naverlabs.com>
2023-01-20 11:10:49 +0100

    Fixed FixedDBN in fixeddbn.yaml from branch fix/fixed-dbn.

--- src/data/simulation/user_model/__init__.py
+++ src/data/simulation/user_model/__init__.py
@@ -1,4 +1,4 @@
 from .carousel import GradedCarousel
-from .dbn import GradedDBN, MixtureDBN
+from .dbn import FixedDBN, GradedDBN, MixtureDBN
 from .pbm import BinaryPBM, GradedPBM
 from .trust import BinaryTrustBias, GradedTrustBias
