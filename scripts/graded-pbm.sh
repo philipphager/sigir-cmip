84de6f706dd5ef95fbe978f1b48084d60e8ea92f
Philipp Hager <p.k.hager@uva.nl>
2023-01-24 00:32:48 +0100

    Add increments

--- scripts/graded-pbm.sh
+++ scripts/graded-pbm.sh
@@ -22,8 +22,12 @@ python main.py -m \
   data/query_dist@data.train_simulator.query_dist="$query_dist" \
   data/query_dist@data.val_simulator.query_dist="$query_dist" \
   data/query_dist@data.test_simulator.query_dist="$query_dist" \
-  data/logging_policy@data.train_policy=noisy-oracle,lambda-mart \
-  data/logging_policy@data.test_policy=uniform \
+  data/logging_policy@data.train_policy=noisy-oracle \
+  data/logging_policy@data.test_policy=uniform,noisy-oracle \
+  data.val_simulator.user_model.random_state_increment=1 \
+  data.test_simulator.user_model.random_state_increment=2 \
+  data.val_simulator.query_dist.random_state_increment=1 \
+  data.test_simulator.query_dist.random_state_increment=2 \
   model=dctr,ranked-dctr,pbm,ubm,dbn,cacm-minus,ncm \
   random_state=30219,76665,88914,11656,43670 \
   $@
