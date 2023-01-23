e07056d281b629a784d8446c9dc74d57c89c2107
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-01-23 14:17:39 +0100

    Add random state increment

--- src/data/simulation/query_dist/uniform.py
+++ src/data/simulation/query_dist/uniform.py
@@ -2,8 +2,14 @@ import torch
 
 
 class UniformQueryDist:
-    def __init__(self, random_state: int):
-        self.generator = torch.Generator().manual_seed(random_state)
+    def __init__(
+        self,
+        random_state: int,
+        random_state_increment: int,
+    ):
+        self.generator = torch.Generator().manual_seed(
+            random_state + random_state_increment
+        )
 
     def __call__(self, n_queries: int, n_sessions: int) -> torch.LongTensor:
         return torch.randint(
